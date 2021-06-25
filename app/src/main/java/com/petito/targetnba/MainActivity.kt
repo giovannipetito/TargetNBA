b0d52c82c4542962b37972de2c5e94b6ad05492e
Giovanni <giovanni.petito88@gmail.com>
2021-06-25 21:17:15 +0200

    first commit

---
+++ app/src/main/java/com/petito/targetnba/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.petito.targetnba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
